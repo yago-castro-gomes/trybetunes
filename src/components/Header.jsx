c11b7e40b1256b1ca748107b9242d4213dd65e0e
Yago Castro <yago.fanstour@gmail.com>
2022-11-03 20:55:36 -0300

    entrega requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { getUser } from '../services/userAPI';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 export default class Header extends Component {
   state = {
@@ -21,6 +22,11 @@ export default class Header extends Component {
     const { loading, user } = this.state;
     return (
       <div>
+        <Link data-testid="link-to-search" to="/search"> Buscar </Link>
+        |
+        <Link data-testid="link-to-favorites" to="/favorites"> Favoritos </Link>
+        |
+        <Link data-testid="link-to-profile" to="/profile"> Perfil </Link>
         <div data-testid="header-component">Header</div>
         { loading ? (<Loading />
         ) : (
